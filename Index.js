eec6c466c358183af644cac9b215c0f77f894da6
Gustav Vang Klinker <guk001@edu.zealand.dk>
2024-11-28 10:35:16 +0100

    Get method

--- Index.js
+++ Index.js
@@ -1,3 +1,5 @@
+const baseUrl = ""
+
 Vue.createApp({
     data() {
         return {
@@ -17,6 +19,21 @@ Vue.createApp({
             } else {
                 this.showFilter = false
             }
+        },
+        getAllMeasurements() {
+            this.getAllMeasurements(baseUrl)
+        },
+        getByLocation(text) {
+            const url = baseUrl + "?location=" + text
+            this.getMeasurements(url)
+        },async getMeasurements(url) {
+            try {
+                const response = await axios.get(url)
+                this.measurements = response.data
+            } catch (ex) {
+                alert(ex.message)
+            }
         }
+
     }
 }).mount("#app")
